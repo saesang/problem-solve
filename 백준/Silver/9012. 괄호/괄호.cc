cefd7bc029dbab30872eb43d622d421a0137dc32
탁세하 <takseha98@gmail.com>
2022-07-13 01:27:51 +0900

    [Silver IV] Title: 괄호, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.cc
+++ 백준/Silver/9012. 괄호/괄호.cc
@@ -1,35 +1,31 @@
 #include<iostream>
-#include<string>
+#include<algorithm>
 #include<stack>
 
 using namespace std;
 
-int main() {
-	int T; cin >> T;
+int main(void) {
+	ios::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
 
+	int T; cin >> T;
 	for (int i = 0; i < T; i++) {
-		stack<char> s; // 괄호 문자열 받을 스택
-		string stmp; cin >> stmp; // 괄호 문자열
-		for (int i = 0; i < stmp.size(); i++) { // 문자열 길이만큼 반복
-			char ctmp; ctmp = stmp[i];
-			if (ctmp == '(') { // '(' 입력되면 스택에 저장
-				s.push(ctmp);
+		stack<char> PS;
+		string ps; cin >> ps; // 괄호 문자열 입력
+
+		// (,) 짝 맞으면 PS.empty() == 1
+		for (int j = 0; j < ps.length(); j++) {
+			if (PS.empty() == 1 && ps[j] == ')') { // )(일 경우, PS.empty() == 0으로 만들고 for문 탈출하도록 설정
+				PS.push(ps[j]);
+				break;
 			}
-			else { 
-				if (s.empty()) { // ')' 입력됐을 때 스택 비어있으면 push해서 스택 채우고 for문 빠져나오기
-					s.push(ctmp);
-					break;
-				}
-				else { // ')' 입력됐을 때 스택 채워져 있으면 '(' pop 시키기
-					s.pop();
-				}
-			}
+			if (ps[j] == '(') PS.push(ps[j]);
+			else PS.pop();
 		}
-		if (s.empty()) { // 스택 비워져 있으면 vps
-			cout << "YES" << '\n';
-		}
-		else {
-			cout << "NO" << '\n';
-		}
+
+		// 결과값 출력
+		if (PS.empty() == 1) cout << "YES" << '\n';
+		else cout << "NO" << '\n';
 	}
+	return 0;
 }
