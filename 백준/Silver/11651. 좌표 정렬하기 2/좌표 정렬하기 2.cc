8dbb063562e75310a8f40a420ba9acb2b5165702
탁세하 <takseha98@gmail.com>
2022-07-19 23:46:06 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 120 ms, Memory: 3572 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.cc
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+vector<pair<int, int>> coord;
+
+int main(void) {
+	int N; cin >> N;
+
+	for (int i = 0; i < N; i++) { // coord에 (x, y) 저장
+		int x, y; cin >> x >> y;
+		coord.push_back(pair<int, int>(y, x));
+	}
+
+	sort(coord.begin(), coord.end());
+
+	for (int i = 0; i < coord.size(); i++) {
+		cout << coord[i].second << ' ' << coord[i].first << '\n';
+	}
+
+	return 0;
+}
