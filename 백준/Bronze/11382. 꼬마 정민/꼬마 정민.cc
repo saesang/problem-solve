8eb5ccc97b0889a60475b296f2916aa8f28873e7
탁세하 <takseha98@gmail.com>
2022-08-23 22:15:37 +0900

    [Bronze V] Title: 꼬마 정민, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.cc
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(void) {
+	ios::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+
+	long long A, B, C; cin >> A >> B >> C;
+
+	cout << A + B + C;
+
+	return 0;
+}
