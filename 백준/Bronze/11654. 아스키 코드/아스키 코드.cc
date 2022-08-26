a113b5fd18de9f82919fa7403079302243a04145
탁세하 <takseha98@gmail.com>
2022-08-26 23:36:53 +0900

    [Bronze V] Title: 아스키 코드, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.cc
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(void) {
+	ios::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+
+	char c; cin >> c;
+	int ASC = int(c);
+	cout << ASC;
+
+	return 0;
+}
