effb27296cdccac7aef20ca7438ddae2964ce9c4
탁세하 <takseha98@gmail.com>
2022-09-01 12:28:10 +0900

    [Bronze II] Title: 블랙잭, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.cc
+++ 백준/Bronze/2798. 블랙잭/블랙잭.cc
@@ -14,15 +14,15 @@ int main(void) {
 		int tmp; cin >> tmp;
 		v.push_back(tmp);
 	}
-	int max = 0; // M 넘지 않는 최댓값
+	int mx = 0; // M 넘지 않는 최댓값
 	for (int i = 0; i < v.size(); i++) {
 		for (int j = i + 1; j < v.size(); j++) {
 			for (int k = j + 1; k < v.size(); k++) {
-				if (v[i]+v[j]+v[k] <= M && v[i] + v[j] + v[k] > max) max = v[i] + v[j] + v[k];
+				if (v[i]+v[j]+v[k] <= M) mx = max(mx, v[i] + v[j] + v[k]);
 			}
 		}
 	}
-	cout << max;
+	cout << mx;
 
 	return 0;
 }
