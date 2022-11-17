d3a8fe63dd3c1b59c37750b80624d5c1d21002ab
탁세하 <takseha98@gmail.com>
2022-11-18 01:03:00 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.cc
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+typedef long long ll;
+
+int main(void) {
+    // 입출력 속도 최적화
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    string hongJun;cin>>hongJun;
+    cout<<":fan::fan::fan:\n:fan::"<<hongJun<<"::fan:\n:fan::fan::fan:";
+
+    return 0;
+}
