b28c2ffeb367b3b39c62af814e2491920cabdb2b
탁세하 <takseha98@gmail.com>
2022-11-19 00:31:46 +0900

    [Bronze V] Title: 나는 누구인가, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15733. 나는 누구인가/나는 누구인가.cc
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+typedef long long ll;
+
+int main(void) {
+    // 입출력 속도 최적화
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+ 
+    cout<<"I'm Sexy";
+    
+    return 0;
+}
