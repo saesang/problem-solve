c3887778eaa8a84d2999f3227e07d3840c28fd1e
탁세하 <takseha98@gmail.com>
2022-11-20 00:08:41 +0900

    [Bronze I] Title: 초콜릿 자르기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2163. 초콜릿 자르기/초콜릿 자르기.cc
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+typedef long long ll;
+
+int main(void) {
+    // 입출력 속도 최적화
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+ 
+    int N,M;cin>>N>>M;
+    cout<<N*M-1;
+    
+    return 0;
+}
