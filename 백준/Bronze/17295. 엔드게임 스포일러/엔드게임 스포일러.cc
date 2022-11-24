40ce96703010cf2bf04a45cf54095a25f9efaa0f
탁세하 <takseha98@gmail.com>
2022-11-25 00:53:51 +0900

    [Bronze V] Title: 엔드게임 스포일러, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/17295. 엔드게임 스포일러/엔드게임 스포일러.cc
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+typedef long long ll;
+
+int main(void) {
+    // 입출력 속도 최적화
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    string s;getline(cin,s);
+    cout<<"Avengers: Endgame";
+
+    return 0;
+}
