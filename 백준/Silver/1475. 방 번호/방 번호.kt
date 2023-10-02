0a493b6376d22d6312a3f60cc25325189099cffd
탁세하 <takseha98@gmail.com>
2023-10-02 10:08:52 +0900

    [Silver V] Title: 방 번호, Time: 96 ms, Memory: 12760 KB -BaekjoonHub

---
+++ 백준/Silver/1475. 방 번호/방 번호.kt
@@ -0,0 +1,32 @@
+import kotlin.math.ceil
+
+fun main() {
+    val N = readlnOrNull() ?: return
+    var cnts = Array(10) { 0 }
+    var mxCnt = 0
+    var result = 0
+
+    for (i in N.indices) {
+        if (N[i].digitToInt() == 6 || N[i].digitToInt() == 9) {
+            cnts[6]++   // 6 또는 9 나오면 cnts[6]에 합쳐서 카운트
+        } else {
+            cnts[N[i].digitToInt()]++
+        }
+    }
+
+    if ((cnts[6] % 2) == 0) {
+        cnts[6] = cnts[6] / 2
+    } else {
+        cnts[6] = cnts[6] / 2 + 1
+    }
+
+    for (i in cnts.indices) {
+        if (mxCnt < cnts[i]) {
+            mxCnt = cnts[i]
+        }
+    }
+
+    result = mxCnt
+
+    println(result)
+}
