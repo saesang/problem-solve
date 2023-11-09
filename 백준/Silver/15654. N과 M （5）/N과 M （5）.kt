4b6c89cf84eb71067d520dbc54a5da20675c58ee
탁세하 <takseha98@gmail.com>
2023-11-09 14:33:03 +0900

    [Silver III] Title: N과 M (5), Time: 584 ms, Memory: 87192 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.kt
@@ -0,0 +1,37 @@
+lateinit var nums: Array<Int>   // 주어진 서로 다른 N개의 정수 배열
+lateinit var visit: HashMap<Int, Boolean>   // key가 N일 때, value가 Boolean인 HashMap
+lateinit var sequence: Array<Int>   //  출력 수열
+lateinit var sequenceStr: StringBuilder // 출력될 결과값
+
+fun dfs(N: Int, M: Int, depth: Int) {
+    if (depth == M) {
+        sequence.forEach { sequenceStr.append("$it ") }
+        sequenceStr.append("\n")
+
+        return
+    }
+
+    for (num in nums) {
+        if (visit[num] == false) {
+            visit[num] = true
+            sequence[depth] = num
+        } else continue
+        dfs(N, M, depth + 1)
+        visit[num] = false
+    }
+}
+
+fun main() {
+    val (N, M) = readln().split(" ").map { it.toInt() }
+    nums = readln().split(" ").map { it.toInt() }.toTypedArray().sortedArray()
+    visit = HashMap()
+    for (num in nums) {
+        visit[num] = false
+    }
+    sequence = Array(M) { 0 }
+    sequenceStr = StringBuilder()
+
+    dfs(N, M, 0)
+
+    print(sequenceStr)
+}
