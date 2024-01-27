79782c5be5b940ec8ef06b3c8ba48fb1ac450c9b
탁세하 <takseha98@gmail.com>
2024-01-27 23:59:34 +0900

    refactor: 등수 구하기.kt

--- 백준/Silver/1205. 등수 구하기/등수 구하기.kt
+++ 백준/Silver/1205. 등수 구하기/등수 구하기.kt
@@ -1,16 +1,22 @@
 fun main() {
     val (n,t,p) = readln().split(" ").map { it.toInt() }
-    var result = -1
+    var result = 0
     if (n == 0) {
         result = 1
     } else {
         val rank = readln().split(" ").map { it.toInt() }
 
-        for (idx in rank.indices) {
-            if (result < 0 && t >= rank[idx]) result = idx + 1
+        if (t < rank[n - 1]) {
+            result = if (n == p) -1 else n + 1
+        } else {
+            for (idx in rank.indices) {
+                if (t >= rank[idx]) {
+                    result = idx + 1
+                    break
+                }
+            }
+            if (t == rank[n - 1] && n == p) result = -1
         }
-        if (result < 0 && n < p) result = n + 1
-        else if (n == p && rank[n - 1] == t) result = -1
     }
 
     println(result)
