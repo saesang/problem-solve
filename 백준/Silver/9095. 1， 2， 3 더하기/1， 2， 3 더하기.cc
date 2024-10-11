35edc6e1e42874b9cef3498c541986ab5b2782e8
탁세하 <takseha98@gmail.com>
2024-10-11 14:24:54 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.cc
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.cc
@@ -1,18 +1,40 @@
-#include<iostream>
+#include <iostream>
+#include <algorithm>
 
 using namespace std;
 
-int memo[1000];
+/*
+ * d[i]: i를 1,2,3의 합으로 나타내는 방법의 수
+ *
+ * d[1] = 1
+ * d[2] = 2
+ * d[3] = 4
+ * d[4] = 7
+ * d[5] = 13
+ *
+ * => d[i] = d[i-1] + d[i-2] + d[i-3]
+ */
 
-// 점화식 f[n] = f[n-1] + f[n-2] + f[n-3]을 사용한다.
+int T;
+int d[20];
+
 int main() {
-	int T; cin >> T;
-	for (int i = 0; i < T; i++) {
-		int n; cin >> n;
-		memo[1] = 1; memo[2] = 2; memo[3] = 4;
-		for (int i = 4; i <= n; i++) {
-			memo[i] = memo[i - 1] + memo[i - 2] + memo[i - 3]; // 점화식
-		}
-		cout << memo[n]<< '\n';
-	}
+    ios::sync_with_stdio();
+    cin.tie(); cout.tie();
+
+    cin >> T;
+
+    for (int i = 0; i < T; i++) {
+        int n;
+        cin >> n;
+
+        d[1] = 1;
+        d[2] = 2;
+        d[3] = 4;
+        for (int j = 4; j <= n; j++) {
+            d[j] = d[j-1] + d[j-2] + d[j-3];
+        }
+
+        cout << d[n] << '\n';
+    }
 }
