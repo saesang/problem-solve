d150d71673520555a76cf6dc28413aa9bdbea51e
탁세하 <takseha98@gmail.com>
2024-10-13 18:15:34 +0900

    [Silver I] Title: 그림, Time: 16 ms, Memory: 4052 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.cc
+++ 백준/Silver/1926. 그림/그림.cc
@@ -4,33 +4,20 @@
 
 using namespace std;
 
-/* 풀이 순서
- *
- * bfs를 통해 현재 그림의 넓이를 확인
- * cnt +1
- * 현재 그림의 넓이를 mx와 비교해서 mx보다 큰 경우에 mx 갱신
- * 다음 그림 탐색
- */
+int n,m;
+int board[510][510];
+int vis[510][510];
+queue<pair<int, int>> q;
 
-/* 다른 그림 탐색 로직 -> 이중 for문으로 처리
- *
- */
+int dx[] = {-1, 0, 1, 0};
+int dy[] = {0, 1, 0, -1};
 
-#define X first // 세미콜론 없애기!
-#define Y second
-
-int board[502][502];
-bool vis[502][502]; // 전역변수로 선언 시 기본적으로 false로 초기화 됨 -> 따로 초기화 필요 x
-int n, m;
-int dx[4] = {1, 0, -1, 0};
-int dy[4] = {0, 1, 0, -1};
-
-int num = 0; // 그림의 개수
-int mx = 0; // 최대 그림의 넓이
+int cnt;
+int mx;
 
 int main() {
-    ios::sync_with_stdio();
-    cin.tie(); cout.tie();
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
 
     cin >> n >> m;
     for (int i = 0; i < n; i++) {
@@ -41,27 +28,32 @@ int main() {
 
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) {
-            if (vis[i][j] || board[i][j] == 0) continue;
-            queue<pair<int, int>> q;
-            q.emplace(i,j);
-            num++;
-            vis[i][j] = true;
-            int sz = 1; // 시작 지점도 넓이에 포함이므로
-            while (!q.empty()) {
-                pair<int, int> cur = q.front(); q.pop();
-                for (int l = 0; l < 4; l++) {
-                    int nx = cur.X + dx[l];
-                    int ny = cur.Y + dy[l];
-                    if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
-                    if (vis[nx][ny] || board[nx][ny] != 1) continue;
-                    q.emplace(nx,ny);
-                    sz++;
-                    vis[nx][ny] = true;
+            if (board[i][j] == 1 && vis[i][j] == 0) {
+                q.push({i, j});
+                vis[i][j] = 1;
+                cnt++;
+
+                int pic = 1;
+                while(!q.empty()) {
+                    pair<int, int> tmp = q.front(); q.pop();
+                    for (int k = 0; k < 4; k++) {
+                        int nx = tmp.first + dx[k];
+                        int ny = tmp.second + dy[k];
+
+                        if (nx >= 0 && nx < n && ny >= 0 && ny < m) {
+                            if (board[nx][ny] == 1 && vis[nx][ny] == 0) {
+                                q.push({nx, ny});
+                                vis[nx][ny] = 1;
+                                pic++;
+                            }
+                        }
+                    }
                 }
+
+                mx = max(pic, mx);
             }
-            mx = max(sz, mx);
         }
     }
 
-    cout << num << '\n' << mx;
+    cout << cnt << '\n' << mx;
 }
